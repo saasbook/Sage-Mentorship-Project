da60867b12b74818d1df0abdfa1c44dd115e4d71
nicholasRuhman <nwruhman@berkeley.edu>
2019-11-14 09:44:29 -0800

    Add figaro encryption. Move geocoder api key to figaro file.

--- config/initializers/google_sign_in.rb
+++ config/initializers/google_sign_in.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  config.google_sign_in.client_id = "874482834971-mcve3h193kpqns4k3n7b8li5k05rb3a8.apps.googleusercontent.com"
-  config.google_sign_in.client_secret = "83BTYWOJ9RHLusbtbVls6CrU"
+  config.google_sign_in.client_id = Figaro.env.google_sign_in_client_id
+  config.google_sign_in.client_secret = Figaro.env.google_sign_in_client_secret
 end
