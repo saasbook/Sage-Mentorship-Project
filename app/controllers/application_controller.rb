51209bcc3bfe83d45c7cd73168c6c5b2bd311a92
nicholasRuhman <nwruhman@berkeley.edu>
2019-11-17 14:32:02 -0800

    remove https redirect. Causes heroku to crash.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  def https_redirect
-    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
-  end
-  before_filter :https_redirect
 end
