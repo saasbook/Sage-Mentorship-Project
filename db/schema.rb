9c48fb1d2022f0e403c39974b9030b41c2030c84
nicholasRuhman <nwruhman@berkeley.edu>
2019-12-11 13:27:47 -0800

    schema and travis.yml updated.

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_032443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
