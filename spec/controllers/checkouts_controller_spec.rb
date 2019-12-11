aaf5ce61296c887d0eb1596e3fbaddaecd9dcf5b
nicholasRuhman <nwruhman@berkeley.edu>
2019-12-11 11:44:08 -0800

    rspec mentor factor created and began editing the checkout_controller_spec

--- spec/controllers/checkouts_controller_spec.rb
+++ spec/controllers/checkouts_controller_spec.rb
@@ -24,22 +24,21 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe CheckoutsController, type: :controller do
-
+  before(:all) do
+    @mentor1 =  Mentor.find_by(email: 'mentorspec1@superspec.berkeley.edu') || create(:mentor, :name => 'mentorspec1', :email => 'mentorspec1@superspec.berkeley.edu')
+    @admin1 =  Admin.find_by(email: 'adminspec1@superspec.berkeley.edu') || create(:admin, :name => 'adminspec1', :email => 'adminspec1@superspec.berkeley.edu')
+  end
   # This should return the minimal set of attributes required to create a valid
   # Checkout. As you add validations to Checkout, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {la: 37.876869, lo: -122.270348, mentor_id: @mentor1.id}
   }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # CheckoutsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { {:email_address => @admin1.email} }
 
   describe "GET #index" do
     it "returns a success response" do
