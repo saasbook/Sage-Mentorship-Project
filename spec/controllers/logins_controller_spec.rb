557d6b4373ea485cd3b7967d7b270a34622a79b9
nicholasRuhman <nwruhman@berkeley.edu>
2019-12-11 00:14:39 -0800

    remove rspec helpers. fix logins_controller rspec file.

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe LoginsController, type: :controller do
+  before(:all) do
+    @super1 =  Super.find_by(email: 'superspec1@superspec.berkeley.edu') || create(:super, :name => 'rspec1', :email => 'superspec1@superspec.berkeley.edu')
+  end
 
-  describe "GET #root" do
-    it "returns http success" do
-      get :root
-      expect(response).to have_http_status(:success)
+  let(:valid_session) { {:user_id => 1, :id => 1, :email_address => @super1.email} }
+
+  describe "GET #destroy" do
+    it "redirects to root" do
+      get :destroy, params: {}, session: valid_session
+      expect(session[:user_id]).to be(nil)
+      expect(session[:email_address]).to be(nil)
+      expect(session[:id]).to be(nil)
+      expect(response).to redirect_to(:root)
     end
   end
 
